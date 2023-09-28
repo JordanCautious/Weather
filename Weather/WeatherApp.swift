957c915b40ebde65a6faea627153a842e6fa74d5
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-28 16:38:18 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jordan Haynes on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
